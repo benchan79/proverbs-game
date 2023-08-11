f0f0031f10d4d97e1630961608df28e50fc6e96b
Ben Chan <benchan79@gmail.com>
2023-08-11 10:06:22 +0800

    Add default page

---
+++ src/DefaultPage.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import { useState } from "react";
+
+function DefaultPage() {
+  const [watchVideo, setWatchVideo] = useState(false);
+  return (
+    <div style={{ textAlign: "center", marginLeft: 40, fontSize: 25 }}>
+      <p>You have reached the end of the internet!</p>
+      <Link to="/proverbs-game/">
+        Return Home or...
+      </Link>
+      <p />
+      <Button
+        onClick={() => setWatchVideo(true)}
+        label="Watch an interesting video"
+      />
+      <p />
+      {watchVideo && (
+        <video
+          controls
+          autoPlay
+          muted={false}
+          src="https://fwesh.yonle.repl.co/"
+        />
+      )}
+    </div>
+  );
+}
+
+export default DefaultPage;
