14246aefbd82cc50d585a34b9f8849ec91abe23c
Ben Chan <benchan79@gmail.com>
2023-08-11 10:08:43 +0800

    Add new routes for homepage and word game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProverbsGame from "./ProverbsGame"
+import Header from './Header';
+import DefaultPage from './DefaultPage';
+import Home from './Home';
+import NumberGame from "./NumberGame"
+import WordGame from "./WordGame"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <main>
         <Routes>
-          <Route path="/proverbs-game" element={<ProverbsGame />} />
+          <Route path="*" element={<DefaultPage />} />
+          <Route path="/proverbs-game" element={<Home />} />
+          <Route path="/proverbs-game/number" element={<NumberGame />} />
+          <Route path="/proverbs-game/word" element={<WordGame />} />
         </Routes>
       </main>
     </BrowserRouter>
